4fc4167e033ed1f0bc508cafa1370bcc957267c0
Eric Ke <26385966+ericke8@users.noreply.github.com>
2019-09-13 23:51:18 -0700

    logging info

--- src/handlers/getThing.js
+++ src/handlers/getThing.js
@@ -4,7 +4,7 @@ const logger = log4js.getLogger();
 
 const getThing = (req, res) => {
   const requestBody = req.body;
-  logger.info("Get request made");
+  logger.info(`Get request made with body ${JSON.stringify(requestBody)}`);
   res.sendStatus(200);
 };
 
--- src/handlers/postThing.js
+++ src/handlers/postThing.js
@@ -4,7 +4,7 @@ const logger = log4js.getLogger();
 
 const postThing = (req, res) => {
   const requestBody = req.body;
-  logger.info("Post request made");
+  logger.info(`Post request made with body ${JSON.stringify(requestBody)}`);
   res.json(requestBody);
 };
 
