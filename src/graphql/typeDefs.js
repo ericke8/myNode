aea3e5566627ebcd2b2e790756eb597bb7172fae
Eric Ke <keric.yi@gmail.com>
2019-09-24 12:50:31 -0700

    graphql querying

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -3,9 +3,10 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
 type Query {
   getThing(id: ID!): Thing!
+  getFirst10Things: [Thing!]
 }
 
-type Things {
+type Thing {
   id: ID!
   description: String!
   createdAt: String!
